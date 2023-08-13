a736234509bc1d3a55a16e45f457f82aa917f67d
dfox <dfox@foxartbox.com>
2023-08-13 15:37:15 +0300

    default vue. Part 6

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -1,16 +1,51 @@
 <template>
-	<div class="header">
-	</div>
+	<div class="header"></div>
 </template>
 
 <script>
+import Konva from 'konva';
+
+// const width = window.innerWidth;
+// const height = window.innerHeight;
 
 export default {
 	name: 'HeaderMain',
-	created() {},
-	data() {},
+	created() {
+		// first we need to create a stage
+		let stage = new Konva.Stage({
+			container:  'container',
+			width:      500,
+			height:     500
+		});
+
+		// then create layer
+		var layer = new Konva.Layer()
+
+		// create our shape
+		var circle = new Konva.Circle({
+			x: stage.width() / 2,
+			y: stage.height() / 2,
+			radius: 70,
+			fill: 'red',
+			stroke: 'black',
+			strokeWidth: 4
+		});
+
+// add the shape to the layer
+		layer.add(circle);
+
+// add the layer to the stage
+		stage.add(layer);
+
+// draw the image
+		layer.draw();
+	},
+	data() {
+	},
 	methods: {},
-	mounted() {},
-	unmounted() {}
+	mounted() {
+	},
+	unmounted() {
+	}
 }
 </script>
